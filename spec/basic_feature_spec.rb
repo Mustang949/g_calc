1e24aabfe526e7fdb8544d3dd6545e938907ad54
Mustang949 <marc_perl@yahoo.com>
2019-09-03 11:36:51 -0700

    First

---
+++ spec/basic_feature_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.feature 'Within the Calcualtor basic functions work correctly' do
+  scenario 'addition' do
+    visit '/search?q=calculator'
+    
+  
+end
---
+++ spec/support/calc_page.rb
@@ -0,0 +1,4 @@
+class CalcPage
+  # https://www.alanfoster.me/posts/feature-testing-with-capybara-page-objects/
+
+end
